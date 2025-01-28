842f03ae788c135124581bb8859eee11d5dee994
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2025-01-27 21:51:32 -0500

    Issues with recently played scope requests. Coming back tomorrow once token expires

--- index.js
+++ index.js
@@ -29,56 +29,83 @@ const getAccessToken = async (client_id, client_secret, refreshToken) => {
       grant_type: "refresh_token",
       refresh_token: refreshToken,
     },
+    scope: 'user-read-currently-playing user-read-recently-played', // check tomorrow
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       Authorization: `Basic ${basic}`,
-    },
-  })
+    }
+  });
 
+  console.log(response.data)
   return response.data;
+};
+
+const getRecentlyPlayed = async (access_token) => {
+  try {
+
+    const wasPlayingResponse = await axios.get(
+      // Get currently playing song using access token
+      "https://api.spotify.com/v1/me/player/recently-played",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        }
+      }
+    );
+
+    console.log(wasPlayingResponse.data)
+  } catch (error) {
+    console.error("Error fetching currently playing song: ", error);
+    return error.message.toString();
+  }
 };
 
 const getNowPlaying = async () => {
   try {
+    //Generate an access token
     const { access_token } = await getAccessToken(
       client_id,
       client_secret,
       refreshToken
     );
 
-    const nowPlayingResponse = await axios.get( // Get currently playing song
-      "https://api.spotify.com/v1/me/player/currently-playing",
+    const nowPlayingResponse = await axios.get(
+      "https://api.spotify.com/v1/me/player/currently-playing", // Get currently playing song using access token
       {
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
         params: {
-            json: true
-          },
+          json: true,
+        },
       }
     );
 
-    if(nowPlayingResponse.status > 400){ //If response status > 400 means there was some error while fetching the required information
-        throw new Error('Error fetching song error code: ', nowPlayingResponse.status);
-    }else if(nowPlayingResponse.status == 204){ // The response was fetched but there was no content
+    if (nowPlayingResponse.status > 400) {
+      //If response status > 400 means there was some error while fetching the required information
+      throw new Error(
+        "Error fetching song error code: ",
+        nowPlayingResponse.status
+      );
+    } else if (nowPlayingResponse.status == 204) {
+      // The response was fetched but there was no content
         throw new Error('Currently not playing.')
-        // ================== FETCH LAST SONG PLAYED? ===================
-    }
+        getRecentlyPlayed(access_token);
+      // ================== FETCH LAST SONG PLAYED? ===================
+    } 
+      const songDetails = await nowPlayingResponse;
+      // console.log(songDetails.data);
+      const artist = songDetails.data.item.artists[0].name;
+      const song = songDetails.data.item.name;
+      const songUrl = songDetails.data.item.external_urls.spotify;
+      const album = songDetails.data.item.album.name;
+      const albumImg = songDetails.data.item.album.images[0].url;
+      const isPlaying = songDetails.data.is_playing;
 
-    const songDetails = await nowPlayingResponse;
-    // console.log(songDetails.data);
-    const artist = songDetails.data.item.artists[0].name;
-    const song = songDetails.data.item.name;
-    const songUrl = songDetails.data.item.external_urls.spotify;
-    const album = songDetails.data.item.album.name;
-    const albumImg = songDetails.data.item.album.images[0].url;
-    const isPlaying = songDetails.data.is_playing;
-
-    console.log(
-        " Artist: " + artist+"\n", "Song: " + song+"\n", "Album: " + album+"\n", "isPlaying: " + isPlaying
-    );
-
-
+      console.log(
+          " Artist: " + artist+"\n", "Song: " + song+"\n", "Album: " + album+"\n", "isPlaying: " + isPlaying
+      );
+    
   } catch (error) {
     console.error("Error fetching currently playing song: ", error);
     return error.message.toString();
@@ -90,8 +117,3 @@ getNowPlaying();
 app.listen("3000", () => {
   console.log("Listening on port 3000");
 });
-
-// https://accounts.spotify.com/en/authorize?client_id=98f199a472094fb890032932de1bef0e&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:3000&scope=user-read-currently-playing
-
-// Refresh token
-// AQAotgFS44BoikKWNUJ-UFufZUGGnWmgLtfzbqv0Ewzf70ADfn5WhyCbwmvHoTiAB72oMXNNEfh5FwJWJEFvCSfqDvTl3hgmKAHQBhupPVzafRjrzB-sxs0tue7DopmyFmbH6NFkWqx5HaHNhFIf02yhRgdULOlhxpTHIusokM09YE2WOYrdHRS2x8VTubLL8crxataZ
